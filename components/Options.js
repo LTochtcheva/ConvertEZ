80845731d15b978f6c65f8a516cfb68690a87df8
Lada Tochtcheva <lada.toch@gmail.com>
2017-08-21 19:30:05 -0400

    add option list native-base

--- components/Options.js
+++ components/Options.js
@@ -1,14 +1,59 @@
 import React, { Component } from 'react';
-import { Container, Content, Text } from 'native-base';
+import { StyleSheet } from 'react-native';
+import { Container,
+         Content, Text,
+         List, ListItem,
+         Header
+        } from 'native-base';
+
+const measurements = [
+  {title: 'Temperature'},
+  {title: 'Length'},
+  {title: 'Weight'},
+  {title: 'Area'},
+  {title: 'Speed'}
+]
 
 export default class Options extends Component {
   render() {
     return (
       <Container>
+        <Header style={styles.contentHeader}>
+          <Text style={styles.textHeader}>Select Option</Text>
+        </Header>
         <Content>
-          <Text>List of options</Text>
+          <List style={styles.list}>
+            {measurements.map((option) => {
+              return (
+              <ListItem style={styles.item}>
+                <Text style={styles.itemText}>{option.title}</Text>
+              </ListItem>)
+            })}
+          </List>
         </Content>
       </Container>
             )
   }
 }
+
+const styles = StyleSheet.create({
+  textHeader: {
+    color: 'blue',
+    fontSize: 26
+  },
+  contentHeader: {
+    backgroundColor: '#DDD',
+    justifyContent: 'flex-start'
+  },
+  list: {
+
+  },
+  item: {
+    borderColor: 'blue',
+    margin: 15
+  },
+  itemText: {
+    color: 'blue',
+    fontSize: 22
+  }
+})
