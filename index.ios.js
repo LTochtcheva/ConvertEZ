ca5c8a554aa1130d5ee3240f60eb3dbb41201071
Lada Tochtcheva <lada.toch@gmail.com>
2017-08-21 13:06:44 -0400

    refactor main component

--- index.ios.js
+++ index.ios.js
@@ -1,8 +1,3 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
 
 import React, { Component } from 'react';
 import {
@@ -12,19 +7,12 @@ import {
   View
 } from 'react-native';
 
-export default class ConvertEZ extends Component {
+export default class Main extends Component {
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
+          Main Component
         </Text>
       </View>
     );
@@ -39,15 +27,10 @@ const styles = StyleSheet.create({
     backgroundColor: '#F5FCFF',
   },
   welcome: {
-    fontSize: 20,
+    fontSize: 30,
     textAlign: 'center',
     margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+  }
 });
 
-AppRegistry.registerComponent('ConvertEZ', () => ConvertEZ);
+AppRegistry.registerComponent('ConvertEZ', () => Main);
